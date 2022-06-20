9f005d3f1f89535273361841192d1841617a4f1c
Grace <wangxiaotian2012@gmail.com>
2022-06-19 22:39:14 -0700

    Add my solution based on the hint for Section 5 multiple pointers

--- CodingExercises/2-MultiplePointers-countUniqueValues-Section5.js
+++ CodingExercises/2-MultiplePointers-countUniqueValues-Section5.js
@@ -23,3 +23,27 @@ function countUniqueValues(array) {
   // Return the count
   return count;
 }
+
+// Based on the hint...
+function countUniqueValues(array) {
+  // If array.length is less than 2 (0 or 1), return array.length
+  if (array.length < 2) return array.length;
+  // If array.length >= 2
+  // Declare two pointer indices at the beginning of the array (pointer and pointer +1)
+  let i = 0;
+  let j = 1
+  // Until the second pointer reaches the end of the array
+  while (j < array.length) {
+    // Compare the values at the two pointers
+    // If they are the same, move j up and continue comparing
+    // Else move i up 1 and set the i-th value to equal j-th value
+    if (array[i] === array[j]) {
+      j++;
+    } else{
+      i++;
+      array[i] = array[j];
+    }
+  }
+  // The count is i + 1 because i starts at 0
+  return i + 1;
+}
