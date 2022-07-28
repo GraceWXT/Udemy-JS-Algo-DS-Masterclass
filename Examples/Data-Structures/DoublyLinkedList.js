d9ce6c9951f2b71c04e4984a95b04b0f5d9ebec8
Grace <wangxiaotian2012@gmail.com>
2022-07-27 21:26:46 -0700

    Implement push method for doubly linked list

--- Examples/Data-Structures/DoublyLinkedList.js
+++ Examples/Data-Structures/DoublyLinkedList.js
@@ -12,4 +12,19 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      const oldTail = this.tail;
+      oldTail.next = newNode;
+      newNode.prev = oldTail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
